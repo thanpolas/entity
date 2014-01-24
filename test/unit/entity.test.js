4a082d502064b4a1b5422db4cee9d566d3b1b987
Thanasis Polychronakis <thanpolas@gmail.com>
2014-01-24 23:48:04 +0200

    remove ctor remnands from main test

--- test/unit/entity.test.js
+++ test/unit/entity.test.js
@@ -27,30 +27,6 @@ tests.surface = function(driver, majNum) {
   // The purpose is to provide a unique string so specific tests are
   // run by using the mocha --grep "1.1.1" option.
 
-  suite(majNum + '.0 constructor and inheritance', function() {
-    test(majNum + '.0.1 ctor arguments', function(){
-      var entity = driver.entity.extend(function(arg1){
-        assert.isEqual(arg1, 2, 'Arguments should be passed as is');
-        assert.isEqual(arguments.length, 1, 'There should be only one argument');
-      });
-      entity(2);
-    });
-
-    test(majNum + '.0.2 inheritance', function() {
-      var invoked = 0;
-      var Entity = driver.entity.extend(function(){
-        invoked++;
-      });
-
-      var entityOne = new Entity();
-      var entityTwo = Entity();
-
-      assert.instanceOf(entityOne, Entity, 'Using new keyword');
-      assert.instanceOf(entityTwo, Entity, 'Just invoking');
-      assert.equal(invoked, 2, 'ctor should be invoked twice');
-    });
-  });
-
   suite(majNum + '.1 API Surface', function() {
     var ent;
     setup(function() {
