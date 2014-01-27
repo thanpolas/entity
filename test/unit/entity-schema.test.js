22aa418810ad80f52e2d485ba1038e384aa2610f
Thanasis Polychronakis <thanpolas@gmail.com>
2014-01-27 22:44:27 +0200

    nail down entity-schema tests

--- test/unit/entity-schema.test.js
+++ test/unit/entity-schema.test.js
@@ -20,11 +20,113 @@ teardown(function() {});
 // The purpose is to provide a unique string so specific tests are
 // run by using the mocha --grep "1.1.1" option.
 
+var schema1 = {
+  firstName: {
+    type: 'string',
+  },
+  lastName: {
+    type: 'string',
+  },
+};
+
 suite('5.2 Entity Schema', function() {
   var ent;
   setup(function() {
-    ent = entity();
+    ent = entity.extend();
   });
-  test('5.2.1 ', function(){
+  suite('5.2.1 Entity Schema Add / Get / Rem', function() {
+    test('5.2.1.1 Entity.addSchema() using an object', function() {
+      ent.addSchema(schema1);
+
+      var schema = ent.getSchema();
+      assert.property(schema, 'firstName');
+      assert.property(schema, 'lastName');
+    });
+    test('5.2.1.1.2 Entity.addSchema() using an object appends', function() {
+      ent.addSchema(schema1);
+      ent.addSchema({
+        age: {
+          type: 'number',
+        },
+      });
+      var schema = ent.getSchema();
+      assert.property(schema, 'firstName');
+      assert.property(schema, 'lastName');
+      assert.property(schema, 'age');
+    });
+    test('5.2.1.1.3 Entity.addSchema() using an object and [type] string as value', function() {
+      ent.addSchema({
+        firstName: 'string',
+        lastName: 'string',
+        age: 'number',
+      });
+      var schema = ent.getSchema();
+      assert.property(schema, 'firstName');
+      assert.property(schema, 'lastName');
+      assert.property(schema, 'age');
+      assert.deepPropertyVal(schema, 'firstName.type', 'string');
+      assert.deepPropertyVal(schema, 'lastName.type', 'string');
+      assert.deepPropertyVal(schema, 'age.type', 'number');
+    });
+
+
+
+    test('5.2.1.2 Entity.addSchema() using key and object', function() {
+      ent.addSchema('firstName', {type: 'string'});
+      ent.addSchema('lastName', {type: 'string'});
+      var schema = ent.getSchema();
+      assert.property(schema, 'firstName');
+      assert.property(schema, 'lastName');
+    });
+    test('5.2.1.3 Entity.addSchema() can be chained', function() {
+      ent.addSchema('firstName', {type: 'string'})
+        .addSchema('lastName', {type: 'string'});
+      var schema = ent.getSchema();
+      assert.property(schema, 'firstName');
+      assert.property(schema, 'lastName');
+    });
+    test('5.2.1.3.2 Entity.addSchema() accepts a string (representing type) as second argument', function() {
+      var schema = ent.addSchema('firstName', 'string')
+        .addSchema('lastName', 'string')
+        .addSchema('age', 'number')
+        .getSchema();
+
+      assert.property(schema, 'firstName');
+      assert.property(schema, 'lastName');
+      assert.deepPropertyVal(schema, 'firstName.type', 'string');
+      assert.deepPropertyVal(schema, 'lastName.type', 'string');
+      assert.deepPropertyVal(schema, 'age.type', 'number');
+    });
+
+
+    test('5.2.1.4 Entity.getSchema() default values', function() {
+      ent.addSchema(schema1);
+      var schema = ent.getSchema();
+      assert.deepPropertyVal(schema, 'firstName.name', 'firstName');
+      assert.deepPropertyVal(schema, 'firstName.path', 'firstName');
+      assert.deepPropertyVal(schema, 'firstName.canShow', true);
+      assert.deepPropertyVal(schema, 'firstName.type', 'string');
+    });
+
+
+    test('5.2.1.5 Entity.remSchema() does not affect passed object', function() {
+      ent.addSchema(schema1);
+      ent.remSchema('lastName');
+      assert.property(schema1, 'lastName');
+    });
+    test('5.2.1.6 Entity.remSchema() removes attribute', function() {
+      ent.addSchema(schema1);
+      ent.remSchema('lastName');
+      var schema = ent.getSchema();
+      assert.notProperty(schema, 'lastName');
+    });
+    test('5.2.1.7 Entity.remSchema() can be chained', function() {
+      var schema = ent.addSchema(schema1)
+        .remSchema('lastName')
+        .remSchema('firstName')
+        .getSchema();
+      assert.notProperty(schema, 'lastName');
+      assert.notProperty(schema, 'firstName');
+    });
   });
 });
--- test/unit/entity.test.js
+++ test/unit/entity.test.js
@@ -22,11 +22,15 @@ teardown(function() {});
 
 suite('1.1 API Surface', function() {
   test('1.1.1 Core Methods', function() {
-    assert.isFunction(entity().getSchema, 'Entity should have a "getSchema" method');
     assert.isFunction(entity().method, 'Entity should have a "method" method');
   });
 
   test('1.1.2 Static Methods', function() {
     assert.isFunction(entity.extend, 'Entity should have an "extend" static method');
+    assert.isFunction(entity.method, 'Entity should have a "method" static method');
+    assert.isFunction(entity.getSchema, 'Entity should have a "getSchema" static method');
+    assert.isFunction(entity.addSchema, 'Entity should have a "addSchema" static method');
+    assert.isFunction(entity.remSchema, 'Entity should have a "remSchema" static method');
+
   });
 });
