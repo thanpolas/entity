175aec28c14ca1c57c9fcfc44916209885edf350
Thanasis Polychronakis <thanpolas@gmail.com>
2014-02-01 16:43:16 +0200

    changing inheritance patterns

--- test/unit/entity-ctor-inheritance.test.js
+++ test/unit/entity-ctor-inheritance.test.js
@@ -20,102 +20,39 @@ teardown(function() {});
 // run by using the mocha --grep "1.1.1" option.
 
 suite('2.0 constructor and inheritance', function() {
-  test('2.0.1 ctor arguments', function() {
-    var entityOne = entity.extend();
-
-
-    // var entityOne = entity.extend(function(arg1){
-    //   assert.isEqual(arg1, 2, 'Arguments should be passed as is');
-    //   assert.isEqual(arguments.length, 1, 'There should be only one argument');
-    // });
-    // entityOne(2);
+  test('2.0.1 Extending with a constructor', function(done) {
+    entity.extend(function() {
+      done();
+    });
   });
   test('2.0.1.1 Can extend without a ctor', function(){
     assert.doesNotThrow(entity.extend);
   });
-  test('2.0.2 inheritance', function() {
-    var invoked = 0;
-    var entityParent = entity.extend(function() {
-      this.id = invoked++;
-    });
-
-    var entityOne = new entityParent();
-    var entityTwo = entityParent();
-
-    assert.instanceOf(entityOne, entityParent, 'Using new keyword');
-    assert.instanceOf(entityTwo, entityParent, 'Just invoking');
-    assert.equal(invoked, 2, 'ctor should be invoked twice');
-    assert.equal(entityOne.id, 1, '"id" prop of entityOne should be 1');
-    assert.equal(entityTwo.id, 2, '"id" prop of entityTwo should be 2');
+  test('2.0.2 extend() returns a singleton instance with a ctor that is instanceof', function() {
+    var entityChild = entity.extend();
+    assert.instanceOf(entityChild, entity.constructor);
   });
 });
 
-test('2.0.3 multiple inheritance', function() {
-  var entityOne = entity.extend(function(num){
-    this.num = num;
-  });
-
-  entityOne.prototype.incr = function() {
-    this.num++;
-  };
-
-  var entityTwo = entityOne.extend(function(num, id) {
-    this.id = id;
-  });
-
-  var entityThree = entityTwo.extend();
-
-  var one = entityOne(1);
-  var two = entityTwo(3, 'two');
-  var three = entityThree(6, 'three');
-
-  assert.isEqual(two.num, 3, '"two.num" should have a value');
-  assert.isEqual(one.num, 1, '"one.num"');
-  assert.isEqual(three.num, 6, '"three.num"');
-  assert.isEqual(two.id, 'two', '"two.id"');
-  assert.isEqual(three.id, 'three', '"three.id"');
-  assert.notProperty(one, 'id', '"one.id"');
-
-  two.incr();
-
-  assert.isEqual(two.num, 4, '"two.num" after .incr()');
-  assert.isEqual(three.num, 6, '"three.num" after .incr()');
-  assert.isEqual(one.num, 1, '"one.num" after .incr()');
-});
-
-test('2.0.3.1 multiple inheritance with static arguments', function() {
-  var entityOne = entity.extend(function(num){
-    this.num = num;
-  });
-
-  entityOne.prototype.incr = function() {
-    this.num++;
-  };
-
-  // 9 is a static arg for entityOne ctor.
-  var entityTwo = entityOne.extend(9, function(id) {
-    this.id = id;
-  });
-
-  var one = entityOne(1);
-  var two = entityTwo('two');
-
-  assert.isEqual(two.num, 9, '"two.num" should have a value');
-  assert.isEqual(one.num, 1, '"one.num"');
-  assert.isEqual(two.id, 'two', '"two.id"');
-  assert.notProperty(one, 'id', '"one.id"');
-
-  two.incr();
-
-  assert.isEqual(two.num, 10, '"two.num" after .incr()');
-  assert.isEqual(one.num, 1, '"one.num" after .incr()');
-});
-
-
-
-test('2.0.4 Static methods', function(){
+test('2.0.3 extend() singleton has a reference to the ctor prototype', function() {
   var entityOne = entity.extend();
-  entityOne.staticMethod = function(){};
-  var entityChild = entityOne.extend();
-  assert.notProperty(entityChild, 'staticMethod');
+  assert.property(entityOne, 'prototype');
+  assert.deepStrictEqual(entityOne.prototype, entityOne.constructor.prototype);
+});
+
+test('2.0.4 using instance prototype methods are immediately available', function() {
+  var entityOne = entity.extend();
+  entityOne.prototype.add = function(a, b) { return a + b; };
+
+  assert.isFunction(entityOne.add);
+  assert.equal(2, entityOne.add(1,1));
+});
+
+test('2.0.5 using instance prototype methods are inherited', function() {
+  var entityOne = entity.extend();
+  entityOne.prototype.add = function(a, b) { return a + b; };
+
+  var entityTwo = entityOne.extend();
+  assert.isFunction(entityTwo.add);
+  assert.equal(2, entityTwo.add(1,1));
 });
