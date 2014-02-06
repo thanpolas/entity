272ba1db802ed8c49997cf68af4b3c02284a783f
Thanasis Polychronakis <thanpolas@gmail.com>
2014-02-06 16:31:33 +0200

    change entity surface test

--- test/unit/entity.test.js
+++ test/unit/entity.test.js
@@ -7,7 +7,7 @@ var chai = require('chai');
 // var sinon = require('sinon');
 var assert = chai.assert;
 
-var entity = require('../../');
+var Entity = require('../../');
 
 
 // var noop = function(){};
@@ -22,12 +22,13 @@ teardown(function() {});
 
 suite('1.1 API Surface', function() {
   test('1.1.1 Core Methods', function() {
-    assert.isFunction(entity.constructor, 'Entity core should have a "constructor"');
-    assert.isFunction(entity.extend, 'Entity should have an "extend" static method');
-    assert.notProperty(entity, 'prototype', 'Entity core should not have a "prototype"');
-    assert.notProperty(entity, 'method', 'Entity core should not have a "method" static method');
-    assert.notProperty(entity, 'getSchema', 'Entity core should not have a "getSchema" static method');
-    assert.notProperty(entity, 'addSchema', 'Entity core should not have a "addSchema" static method');
-    assert.notProperty(entity, 'remSchema', 'Entity core should not have a "remSchema" static method');
+    assert.isFunction(Entity, 'Entity core is a "constructor"');
+    assert.isFunction(Entity.extend, 'Entity should have an "extend" static method');
+    assert.isFunction(Entity.getInstance, 'Entity should have a "getInstance" static method');
+    assert.notProperty(Entity, 'prototype', 'Entity core should not have a "prototype"');
+    assert.notProperty(Entity, 'method', 'Entity core should not have a "method" static method');
+    assert.notProperty(Entity, 'getSchema', 'Entity core should not have a "getSchema" static method');
+    assert.notProperty(Entity, 'addSchema', 'Entity core should not have a "addSchema" static method');
+    assert.notProperty(Entity, 'remSchema', 'Entity core should not have a "remSchema" static method');
   });
 });
