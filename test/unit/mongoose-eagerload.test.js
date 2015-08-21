4634648a33309a1ce8fe1cfda754c4c56b7e2bca
Thanasis Polychronakis <thanpolas@gmail.com>
2015-08-21 15:04:43 +0300

    decouple test case setup to mongoose test lib

--- test/unit/mongoose-eagerload.test.js
+++ test/unit/mongoose-eagerload.test.js
@@ -2,7 +2,6 @@
  * @fileOverview Testing Mongoose eagerload methods.
  */
 var __ = require('lodash');
-var Promise = require('bluebird');
 // var sinon  = require('sinon');
 var chai = require('chai');
 var expect = chai.expect;
@@ -10,57 +9,13 @@ var expect = chai.expect;
 // var assert = chai.assert;
 
 var fix = require('../fixture/data.fix');
-var Entity = require('../..');
 var mongStub = require('../lib/mongoose-stub');
 
-suite.only('Mongoose Eager Load Methods', function() {
+suite('Mongoose Eager Load Methods', function() {
   setup(mongStub.connect);
   setup(mongStub.nukedb);
 
-  setup(function() {
-    var EntMong = Entity.Mongoose.extend();
-    this.entity = new EntMong();
-    this.entity.setModel(mongStub.Model);
-
-    var EntRelMong = Entity.Mongoose.extend();
-    this.entityRel = new EntRelMong();
-    this.entityRel.setModel(mongStub.ModelRel);
-    this.entityRel.eagerLoad('parent');
-  });
-  setup(function() {
-    return Promise.all([
-      this.entity.create(fix.one),
-      this.entity.create(fix.two),
-    ])
-      .bind(this)
-      .then(function(res) {
-        this.recordOne = res[0];
-        this.recordTwo = res[1];
-      });
-  });
-  setup(function() {
-    var fixOne = __.clone(fix.relOne);
-    var fixTwo = __.clone(fix.relTwo);
-    fixOne.parent = this.recordOne._id;
-    fixTwo.parent = this.recordTwo._id;
-
-    return Promise.all([
-      this.entityRel.create(fixOne),
-      this.entityRel.create(fixTwo),
-    ])
-      .bind(this)
-      .then(function(res) {
-        this.recordRelOne = res[0];
-        this.recordRelTwo = res[1];
-      });
-  });
-
-  teardown(function() {
-    return Promise.all([
-      this.entity.delete(),
-      this.entityRel.delete(),
-    ]);
-  });
+  mongStub.setupRecords();
 
   suite('Normal Operation', function() {
     function testParent(item) {
